7a9562102581af4684213081042a892e25f52a1e
Barry Gausden <gitkraken@gausden.org>
2020-10-01 20:34:18 +0800

    Introduce local-override CSS * to override materializecss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import materializecssjs from "./vendor/materialize.js";
-import modernizr from "./vendor/modernizr-3.11.2.min.js";
+import localOverride from "../css/local-override.css";
 import maincss from "../css/main.css";
+import modernizr from "./vendor/modernizr-3.11.2.min.js";
+import materializecssjs from "./vendor/materialize.js";
 import materializecss from "../css/materialize.css";
 import normalizecss from "../css/normalize.css";
 import glowlogo from "../img/glow.svg";
-console.log("Hello World from your main file!");

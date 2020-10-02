df70231d5a5026d5d9cbf3e3ad30f8af94bc92c8
Barry Gausden <gitkraken@gausden.org>
2020-10-02 17:18:59 +0800

    Inline CSS into HTML * inlined as <STYLE> instead of in bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,41 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const InlineChunkHtmlPlugin = require("inline-chunk-html-plugin");
 const HtmlWebpackInlineSVGPlugin = require("html-webpack-inline-svg-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin")
+  .default;
 
 const config = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   module: {
     rules: [
-      {
+      /* {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
-      }
-    ]
+      } */
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
+    ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin(),
+    new HTMLInlineCSSWebpackPlugin(),
     new HtmlWebpackInlineSVGPlugin(),
-    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/\.(css|js)$/])
-  ]
+    // new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/\.(css|js)$/])
+  ],
 };
 
 module.exports = config;

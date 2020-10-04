93ab1f1891ba741b0c2fb1bf1ab86a12188ab072
Barry Gausden <barry@lissome.hk>
2020-10-04 22:33:34 +0800

    Functioning PurgeCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,35 @@
 const webpack = require("webpack");
 const path = require("path");
+const glob = require("glob");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const InlineChunkHtmlPlugin = require("inline-chunk-html-plugin");
 const HtmlWebpackInlineSVGPlugin = require("html-webpack-inline-svg-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
 const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin")
   .default;
 
+const PATHS = {
+  src: path.join(__dirname, "src"),
+};
 const config = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true,
+        },
+      },
+    },
   },
   watch: true,
   watchOptions: {
@@ -26,18 +43,18 @@ const config = {
         use: ["style-loader", "css-loader"],
       }, */
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          "css-loader",
-        ],
+        test: /\.css$/i,
+        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"],
       },
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    }),
     new HtmlWebpackPlugin({
       // use raw-loader to prevent webpack trying to resolve ejs fields
       template: "!!raw-loader!" + path.join(__dirname, "src/index.ejs"),
